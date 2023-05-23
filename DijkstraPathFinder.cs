f1a4fd339186d685347f025d7425a87cd33025a0
К Б <yabkn@outlook.com>
2023-05-23 09:30:57 +0700

    permutation for NotGreedyPathFinder

--- DijkstraPathFinder.cs
+++ DijkstraPathFinder.cs
@@ -13,23 +13,17 @@ namespace Greedy
         public IEnumerable<PathWithCost> GetPathsByDijkstra(State state, Point start,
             IEnumerable<Point> targets)
         {
-            var targets2 = targets;
-            HashSet<Point> notVisited = new HashSet<Point>();
-            for (int i = 0; i < state.CellCost.GetLength(0); i++)
-            {
-                for (int j = 0; j < state.CellCost.GetLength(1); j++)
-                {
-                    if (!state.IsWallAt(new Point(i, j))) notVisited.Add(new Point(i, j));
-                }
-            }
+            HashSet<Point> notOpened = new HashSet<Point>();//будем их добавлять лениво
+            notOpened.Add(start);
+            HashSet<Point> visitedNodes = new HashSet<Point>();
             var track = new Dictionary<Point, DijkstraData>();//будет хранится информация об оценках для каждой вершины
             track[start] = new DijkstraData { Cost = 0, Previous = null };//заносим инфу о нулевой вершине
 
             while (true)
             {
-                Point? toOpen = null;//открываемая вершина
+                Point? toOpen = null;//открываемая вершина. делаем типом nullable чтобы можно было null
                 var bestPrice = double.PositiveInfinity;
-                foreach (var e in notVisited)//для каждой вершины в непосещенных
+                foreach (var e in notOpened)//для каждой вершины в непосещенных
                 {
                     if (track.ContainsKey(e) && track[e].Cost < bestPrice)
                     {
@@ -37,27 +31,23 @@ namespace Greedy
                         toOpen = e;
                     }
                 }
-                if (toOpen == null) yield break;
-                if (targets2.Contains((Point)toOpen))
+                if (toOpen == null || targets.Count() == 0) yield break;
+                if (targets.Contains(toOpen.Value))
                 {
                     yield return ConvertResult(track, (Point)toOpen);//выход из бесконечного цикла здесь
-                    targets2 = targets2.Where(x => x != toOpen);
                 }
 
-                foreach (var e in PossiblePoint((Point)toOpen, state))//пройдемся по всем ребрам у которых эта вершина является начальной
+                foreach (var e in PossiblePoint(toOpen.Value, state))//пройдемся по всем ребрам у которых эта вершина является начальной
                 {
-                    var currentPrice = track[(Point)toOpen].Cost + state.CellCost[e.X, e.Y];//плюс вес ребра
+                    var currentPrice = track[toOpen.Value].Cost + state.CellCost[e.X, e.Y];//плюс вес ребра
                     if (!track.ContainsKey(e) || track[e].Cost > currentPrice)//если для вершины не найдена вообще никакая оценка или найденная оценка хуже чем для посчитанной вершины
                     {
                         track[e] = new DijkstraData { Previous = toOpen, Cost = currentPrice };//запишем новую цену и вершину из которой пришли//запишем новую цену и вершину из которой пришли
-                        if (targets2.Contains(e))
-                        {
-                            yield return ConvertResult(track, e);//выход из бесконечного цикла здесь
-                            targets2 = targets2.Where(x => x != e);
-                        }
                     }
+                    if (!visitedNodes.Contains(e)) notOpened.Add(e);
                 }
-                notVisited.Remove((Point)toOpen);
+                notOpened.Remove(toOpen.Value);//или (Point)toOpen
+                visitedNodes.Add(toOpen.Value);
             }
         }
 
@@ -68,8 +58,8 @@ namespace Greedy
             while (true)
             {
                 result.Add(node);
-                if (track[node].Previous==null) break;
-                node = (Point)track[node].Previous;
+                if (track[node].Previous == null) break;
+                node = track[node].Previous.Value;
             }
             result.Reverse();
             return new PathWithCost(track[end].Cost, result.ToArray());
@@ -90,6 +80,16 @@ namespace Greedy
             }
             return result;
         }
+        public IEnumerable<Point> GetIncidentNodes(Point node, State state)
+        {
+            return new Point[]
+            {
+                new Point(node.X, node.Y+1),
+                new Point(node.X, node.Y-1),
+                new Point(node.X+1, node.Y),
+                new Point(node.X-1, node.Y)
+            }.Where(point => state.InsideMap(point) && !state.IsWallAt(point));
+        }
     }
 
     class DijkstraData
@@ -97,6 +97,5 @@ namespace Greedy
         public Point? Previous { get; set; }//как пришли в эту вершину. Можно было обойтись полями т.к. чисто служебный класс
         public int Cost { get; set; }//цена вершины
     }
-
 
 }
--- NotGreedyPathFinder.cs
+++ NotGreedyPathFinder.cs
@@ -2,14 +2,97 @@
 using System.Drawing;
 using Greedy.Architecture;
 using Greedy.Architecture.Drawing;
+using System.Linq;
+using System;
 
 namespace Greedy
 {
 	public class NotGreedyPathFinder : IPathFinder
 	{
 		public List<Point> FindPathToCompleteGoal(State state)
+		{	
+			if (state.Chests.Count == 0 || state.Energy == 0 || state.Chests.Count < state.Goal)
+				return new List<Point>();
+			var paths = new DijkstraPathFinder();
+			var chestArray = state.Chests.ToArray();
+			var trackEval = new Dictionary<string, PathWithCost>();
+			var result = new List<int[]>();
+			MakePermutations(new int[state.Chests.Count], 0, state, result);
+			int bestPrice = int.MaxValue;
+			int[] intItem=new int[state.Chests.Count];
+			int bestChests=int.MinValue;
+			foreach (var item in result)
+            {
+				int price = 0;
+				int chests = 0;
+				Point start = state.Position;
+				Point end = chestArray[0];
+				for (int i = 0; i < item.Length; i++)
+                {
+					end = chestArray[item[i]];
+					chests++;
+					var startEnd = start.ToString() + end.ToString(); 
+					if (!trackEval.ContainsKey(startEnd))
+						trackEval[startEnd] = paths.GetPathsByDijkstra(state, start, new List<Point> { end })
+							.FirstOrDefault();
+					price += trackEval[startEnd].Cost;
+					int y;
+					if (chests == 7)
+						y = 0;
+					if (price > state.Energy|| chests> chestArray.Length+1)
+					{
+						price -= trackEval[startEnd].Cost;
+						chests--;
+						break;
+					}
+					start = end;
+				}
+				if (chests> bestChests)
+				{
+					bestPrice = price;
+					intItem = item;
+					bestChests = chests;
+				}
+				if (bestPrice == state.InitialEnergy && bestChests == chestArray.Length) break;
+			}
+			return CovertResult(intItem, trackEval, state.Position, chestArray, bestChests);
+		}
+
+		static List<Point> CovertResult(int[] intItem, Dictionary<string,PathWithCost> trackEval, 
+			Point start, Point[] chestArray, int chests)
+        {
+			var result = new List<Point>();
+			for (int k = 0; k < chests; k++)
+			{
+				Point end = chestArray[intItem[k]];
+				var startEnd = start.ToString() + end.ToString();
+				foreach (var point in trackEval[startEnd].Path.Skip(1))
+                {
+					result.Add(point);
+				}
+				start = end;
+			}
+			return result;
+		}
+
+		static void MakePermutations(int[] permutation, int countChests,State state, List<int[]> result)
 		{
-			return new List<Point>();
+			if (countChests == permutation.Length)
+			{
+				var arrVar = new int[permutation.Length]; //интересный момент как удалось отвязать ссылку на permutation при копировании в список result
+				for (int i = 0; i < permutation.Length; i++)
+					arrVar[i] = permutation[i];
+				result.Add(arrVar);
+				return;
+			}
+			for (int i = 0; i < permutation.Length; i++)
+			{
+				var index = Array.IndexOf(permutation, i, 0, countChests);
+				if (index != -1)
+					continue;
+				permutation[countChests] = i;
+				MakePermutations(permutation, countChests + 1, state, result);
+			}
 		}
 	}
 }
