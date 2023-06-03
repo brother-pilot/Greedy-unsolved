11a1ff4029b8e34ef042c9022fdf165150d198d1
К Б <yabkn@outlook.com>
2023-06-03 16:37:43 +0700

    have done

--- NotGreedyPathFinder.cs
+++ NotGreedyPathFinder.cs
@@ -7,16 +7,10 @@ using System;
  
 namespace Greedy
 {
-    class DataNotGreedy
-    {
-        public int Price { get; set; }
-        public int Chests { get; set; }
-        //public Point Previous { get; set; }
-        public List<Point> ChestList { get; set; }
-        public List<Point> BestTrack { get; set; }
-    }
+    
     public class NotGreedyPathFinder : IPathFinder
     {
+        //глобальные перменные для хранения найденного самого оптимального пути
         static int bestPrice = int.MaxValue;
         static int bestChests = int.MinValue;
         static List<Point> bestTrack = new List<Point>();
@@ -25,21 +19,20 @@ namespace Greedy
             if (state.Chests.Count == 0 || state.Energy == 0 || state.Chests.Count < state.Goal)
                 return new List<Point>();
             var trackEval = new Dictionary<(Point, Point), PathWithCost>();
-            var pointEval = new Dictionary<(Point, Point), DataNotGreedy>();
-            MakePath(state, trackEval, pointEval, new List<Point>(), state.Position, state.Chests.ToList());
-            return CovertResult(state, pointEval, trackEval);
+            //запускаем полный перебор по сундукам с поиском в глубину 
+            MakePath(state, trackEval, new List<Point>(), state.Position, state.Chests.ToList());
+            return CovertResult(state, trackEval);
         }
 
         void MakePath(State state, Dictionary<(Point, Point), PathWithCost> trackEval,
-            Dictionary<(Point, Point), DataNotGreedy> pointEval, List<Point> track2,
-            Point start,
-            List<Point> chestList, int price = 0, int chests = 0)
+            List<Point> track2, Point start, List<Point> chestList, int price = 0, int chests = 0)
         {
             var paths = new DijkstraPathFinder();
             var chestLocalList = chestList.ToList();
             List<Point> track = track2.ToList(); 
             foreach (var chest in chestList)
             {
+                //уменьшаем число вычислений сохраняя найденне переходы от сундука к сундуку
                 if (!trackEval.ContainsKey((start, chest)))
                 {
                     trackEval[(start, chest)] =
@@ -49,14 +42,7 @@ namespace Greedy
                 price += trackEval[(start, chest)].Cost;
                 track.Add(chest);
                 chestLocalList.Remove(chest);
-                pointEval[(start, chest)] = new DataNotGreedy
-                {
-                    Price = price,
-                    Chests = chests,
-                    ChestList = chestLocalList.ToList(),
-                    
-                    BestTrack= track.ToList()
-            };
+                //проверяем на отсечение получаемый вариант
                 if (price > state.Energy || chests > state.Chests.Count + 1)
                 {
                     price -= trackEval[(start, chest)].Cost;
@@ -64,13 +50,15 @@ namespace Greedy
                     track.Remove(chest);
                     BestChase(price, chests, track);
                     continue;
-                    //break;
                 }
                 BestChase(price, chests, track);
-                MakePath(state, trackEval, pointEval, track,chest, chestLocalList, price, chests);
+                MakePath(state, trackEval, track,chest, chestLocalList, price, chests);
                 chestLocalList.Add(chest);
+                //возращаем все в исходное состояние в этом цикле перебора сундука чтобы цена
+                //и кол-во сундуков были те же
                 price -= trackEval[(start, chest)].Cost;
                 chests--;
+                //возращаем все в исходное состояние в этом цикле перебора сундука чтобы не было циклов
                 track.Remove(chest);
             }
         }
@@ -85,12 +73,10 @@ namespace Greedy
             }
         }
 
-        static List<Point> CovertResult(State state,Dictionary<(Point, Point), DataNotGreedy> pointEval,
-            Dictionary<(Point, Point), PathWithCost> trackEval)
+        static List<Point> CovertResult(State state, Dictionary<(Point, Point), PathWithCost> trackEval)
         {
             var result = new List<Point>();
             var start = state.Position;
-
             foreach (var end in bestTrack)
             {
                 foreach (var point in trackEval[(start,end)].Path.Skip(1))
