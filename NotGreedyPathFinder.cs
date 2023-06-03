3992b95f51e7ab0088579661e41d8b29ead403dd
К Б <yabkn@outlook.com>
2023-06-03 16:28:58 +0700

    with class DataNotGreedy for debugging

--- NotGreedyPathFinder.cs
+++ NotGreedyPathFinder.cs
@@ -11,156 +11,89 @@ namespace Greedy
     {
         public int Price { get; set; }
         public int Chests { get; set; }
-        public Point Previous { get; set; }
-        public List<Point> ChestList { get; set; }   
+        //public Point Previous { get; set; }
+        public List<Point> ChestList { get; set; }
+        public List<Point> BestTrack { get; set; }
     }
     public class NotGreedyPathFinder : IPathFinder
     {
+        static int bestPrice = int.MaxValue;
+        static int bestChests = int.MinValue;
+        static List<Point> bestTrack = new List<Point>();
         public List<Point> FindPathToCompleteGoal(State state)
         {
             if (state.Chests.Count == 0 || state.Energy == 0 || state.Chests.Count < state.Goal)
                 return new List<Point>();
-
-            //var ttt = paths.GetPathsByDijkstra(state, state.Position, chestArray2);
-            //foreach (var item in ttt)
-            //{
-            //    trackEval[state.Position.ToString() + item.End.ToString()] = item;
-            //    if (item.Cost > state.Energy) chestArray2.Remove(item.End);
-            //}
-            //         var result = new List<int[]>();
-            //MakePermutations(new int[chestArray2.Count], 0, state, result);
-
-            //foreach (var item in result)
-            //         {
-            //    int price = 0;
-            //    int chests = 0;
-            //    Point start = state.Position;
-            //    Point end = chestArray2[0];
-            //    for (int i = 0; i < item.Length; i++)
-            //             {
-            //        end = chestArray2[item[i]];
-            //        chests++;
-            //        var startEnd = start.ToString() + end.ToString(); 
-            //        if (!trackEval.ContainsKey(startEnd))
-            //            trackEval[startEnd] = paths.GetPathsByDijkstra(state, start, new List<Point> { end })
-            //                .FirstOrDefault();
-            //        price += trackEval[startEnd].Cost;
-            //        if (price > state.Energy|| chests> chestArray2.Count+1)
-            //        {
-            //            price -= trackEval[startEnd].Cost;
-            //            chests--;
-            //            break;
-            //        }
-            //        start = end;
-            //    }
-            //    if (chests> bestChests)
-            //    {
-            //        bestPrice = price;
-            //        intItem = item;
-            //        bestChests = chests;
-            //    }
-            //    if (bestPrice == state.InitialEnergy && bestChests == chestArray2.Count) break;
-            //}
-
-            //public static IEnumerable<Node> DepthSearch_Correct(Point startNode)
-
-            var paths = new DijkstraPathFinder();
-            var trackEval = new Dictionary<string, PathWithCost>();
-            var pointEval = new Dictionary<string, DataNotGreedy>();
-            var stack = new Stack<Point>();
-            stack.Push(state.Position);
-            Point previousPoint = state.Position;
-            pointEval[state.Position.ToString()+ state.Position.ToString()] = new DataNotGreedy { Price=0,
-                Chests=0, ChestList=state.Chests.ToList(), Previous= state.Position};
-            int bestPrice = int.MaxValue;
-            int[] intItem = new int[state.Chests.Count];
-            Point intPoint = state.Position;
-            int bestChests = int.MinValue;
-            var visited= new HashSet<String>();
-            //visited.Add(state.Position);
-            while (stack.Count != 0)
-            {
-
-                var point = stack.Pop();
-                var startPrev= point.ToString() + previousPoint.ToString();
-                int price = pointEval[startPrev].Price;
-                int chests = pointEval[startPrev].Chests;
-                var chestArray2 = pointEval[startPrev].ChestList.ToList(); //благодаря toList отвязываем копирование по ссылке
-                chestArray2.Remove(point);
-                //yield return point;
-                //result.Add(point);
-                List<PathWithCost> possible = new List<PathWithCost>();
-                if (!visited.Contains(point.ToString()))
-                    possible = paths.GetPathsByDijkstra(state, point, chestArray2).ToList();
-                else {
-                    continue;
-                    foreach (var item in chestArray2)
-                    {
-                        var yy = item.ToString() + point.ToString();
-                        var ttt = trackEval[item.ToString() + point.ToString()];
-                        possible.Add(ttt);
-                    }
-                        }
-                visited.Add(point.ToString());
-                //var chestArray3=possible.
-                //foreach (var nextPoint in paths.GetPathsByDijkstra(state, point, chestArray2).Where(n => !visited.Contains(n.End)))
-                foreach (var nextPoint in possible.Select(x=>x.End))
-                {
-                    //visited.Add(nextPoint.End);
-                    var chestArray = chestArray2.ToList();
-                    price = pointEval[startPrev].Price;
-                    chests = pointEval[startPrev].Chests;
-                    stack.Push(nextPoint);
-                    var startEnd = point.ToString() + nextPoint.ToString();
-                    var endStart = nextPoint.ToString() + point.ToString();
-                    if (!trackEval.ContainsKey(startEnd))
-                    {
-                        trackEval[startEnd] = possible.Where(x=>x.End==nextPoint).FirstOrDefault();
-                        trackEval[endStart] = possible.Where(x => x.End == nextPoint).FirstOrDefault();
-                    }
-                    //else break;
-                    chests++;
-                    price += trackEval[startEnd].Cost;
-                    chestArray.Remove(nextPoint);
-                    pointEval[nextPoint.ToString()+point.ToString()] = new DataNotGreedy { Price = price, 
-                        Chests = chests, ChestList = chestArray.ToList(), Previous = point };
-                    if (price > state.Energy || chests > chestArray.Count + 1)
-                    {
-                        price -= trackEval[startEnd].Cost;
-                        chests--;
-                        break;
-                    }
-                    if (chests > bestChests)
-                    {
-                        bestPrice = price;
-                        intPoint = stack.Peek();
-                        //intItem = item;
-                        bestChests = chests;
-                    }
-
-                }
-                if (chests > bestChests)
-                {
-                    bestPrice = price;
-                    intPoint = stack.Peek();
-                    //intItem = item;
-                    bestChests = chests;
-                }
-                previousPoint = pointEval[startPrev].Previous;
-            }
-
-            return CovertResult(intPoint, trackEval, state.Position, state.Chests.ToArray(), bestChests);
+            var trackEval = new Dictionary<(Point, Point), PathWithCost>();
+            var pointEval = new Dictionary<(Point, Point), DataNotGreedy>();
+            MakePath(state, trackEval, pointEval, new List<Point>(), state.Position, state.Chests.ToList());
+            return CovertResult(state, pointEval, trackEval);
         }
 
-        static List<Point> CovertResult(Point intPoint, Dictionary<string, PathWithCost> trackEval,
-            Point start, Point[] chestArray, int chests)
+        void MakePath(State state, Dictionary<(Point, Point), PathWithCost> trackEval,
+            Dictionary<(Point, Point), DataNotGreedy> pointEval, List<Point> track2,
+            Point start,
+            List<Point> chestList, int price = 0, int chests = 0)
+        {
+            var paths = new DijkstraPathFinder();
+            var chestLocalList = chestList.ToList();
+            List<Point> track = track2.ToList(); 
+            foreach (var chest in chestList)
+            {
+                if (!trackEval.ContainsKey((start, chest)))
+                {
+                    trackEval[(start, chest)] =
+                    paths.GetPathsByDijkstra(state, start, new List<Point> { chest }).FirstOrDefault();
+                }
+                chests++;
+                price += trackEval[(start, chest)].Cost;
+                track.Add(chest);
+                chestLocalList.Remove(chest);
+                pointEval[(start, chest)] = new DataNotGreedy
+                {
+                    Price = price,
+                    Chests = chests,
+                    ChestList = chestLocalList.ToList(),
+                    
+                    BestTrack= track.ToList()
+            };
+                if (price > state.Energy || chests > state.Chests.Count + 1)
+                {
+                    price -= trackEval[(start, chest)].Cost;
+                    chests--;
+                    track.Remove(chest);
+                    BestChase(price, chests, track);
+                    continue;
+                    //break;
+                }
+                BestChase(price, chests, track);
+                MakePath(state, trackEval, pointEval, track,chest, chestLocalList, price, chests);
+                chestLocalList.Add(chest);
+                price -= trackEval[(start, chest)].Cost;
+                chests--;
+                track.Remove(chest);
+            }
+        }
+
+        static void BestChase(int price, int chests, List<Point> track)
+        {
+            if (chests > bestChests)
+            {
+                bestPrice = price;
+                bestTrack=track.ToList();
+                bestChests = chests;
+            }
+        }
+
+        static List<Point> CovertResult(State state,Dictionary<(Point, Point), DataNotGreedy> pointEval,
+            Dictionary<(Point, Point), PathWithCost> trackEval)
         {
             var result = new List<Point>();
-            for (int k = 0; k < chests; k++)
+            var start = state.Position;
+
+            foreach (var end in bestTrack)
             {
-                Point end = intPoint;
-                var startEnd = start.ToString() + end.ToString();
-                foreach (var point in trackEval[startEnd].Path.Skip(1))
+                foreach (var point in trackEval[(start,end)].Path.Skip(1))
                 {
                     result.Add(point);
                 }
@@ -168,7 +101,5 @@ namespace Greedy
             }
             return result;
         }
-
-
     }
 }
